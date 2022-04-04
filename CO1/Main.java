0b5e21e4a000889fcd75d3ac4bd81f513b1641f5
SHALU SHANAVAS <97325194+Shalushanavas@users.noreply.github.com>
2022-04-04 14:11:56 +0530

    LAB PROGRAMS

---
+++ CO1/Main.java
@@ -0,0 +1,46 @@
+class Main
+{
+	int pcode;
+	String pname;
+	double price;
+	double lowest;
+	void data(int c,String n,double p)
+	{
+		pcode=c;
+		pname=n;
+		price=p;
+	}
+	void display()
+	{
+		System.out.println(pcode+"\t\t"+pname+"\t\t"+price);
+	}
+	static void findLowest(double price1,double price2,double price3)
+	{
+		if(price1<=price2 && price1<=price3)
+	{
+		System.out.println("\n product 2 is the lowest price");
+	}
+	else
+	{
+		System.out.println("\n product 3 is the lowest price");
+	}
+	}
+public static void main(String[]args)
+	{
+	Main obj1=new Main();
+	Main obj2=new Main();
+	Main obj3=new Main();
+	obj1.data(101,"product_1",100.0);
+	obj2.data(102,"product_2",128.40);
+	obj3.data(103,"product_3",790.00);
+	System.out.println("\nproduct information:\nproduct code\tproduct_name\tproduct_price");
+	obj1.display();
+	obj2.display();
+	obj3.display();
+	findLowest(obj1.price,obj2.price,obj3.price);
+}
+}
+
+	
+		
+	
